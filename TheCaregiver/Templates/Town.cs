4968283ab94724d797f178c112dcb5b5eff61c4b
M Michener <60799555+mmichener@users.noreply.github.com>
2021-02-09 12:02:13 -0500

    Replaced hardcoded town data with json file

--- TheCaregiver/Templates/Town.cs
+++ TheCaregiver/Templates/Town.cs
@@ -11,6 +11,14 @@ namespace TheCaregiver.Classes
     {
         public string name { get; set; }
         public int id { get; set; }
+        public int startX { get; set; }
+        public int startY { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int worldX { get; set; }
+        public int worldY { get; set; }
+        public string place { get; set; }
+        public string filePath { get; set; }
         public List<Location> locations { get; set; }
     }
 }
